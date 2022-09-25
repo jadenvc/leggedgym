9f08ce2c3582963ac408e5b466509d5e866441c0
Wenhao Yu <magicmelon@google.com>
2022-09-25 00:16:12 -0700

    Add Pupper model and make sure we can see it

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -38,6 +38,7 @@ from .anymal_b.anymal_b_config import AnymalBRoughCfg, AnymalBRoughCfgPPO
 from .cassie.cassie import Cassie
 from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
+from .pupper.pupper_config import PupperFlatCfg, PupperFlatCfgPPO
 
 
 import os
@@ -49,3 +50,4 @@ task_registry.register( "anymal_c_flat", Anymal, AnymalCFlatCfg(), AnymalCFlatCf
 task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPPO() )
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
+task_registry.register( "pupper_flat", LeggedRobot, PupperFlatCfg(), PupperFlatCfgPPO() )
---
+++ legged_gym/envs/pupper/pupper_config.py
@@ -0,0 +1,92 @@
+# SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Copyright (c) 2021 ETH Zurich, Nikita Rudin
+
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
+
+class PupperFlatCfg( LeggedRobotCfg ):
+    class env( LeggedRobotCfg.env ):
+        num_observations = 48
+  
+    class terrain( LeggedRobotCfg.terrain ):
+        mesh_type = 'plane'
+        measure_heights = False
+        
+    class init_state( LeggedRobotCfg.init_state ):
+        pos = [0.0, 0.0, 0.5] # x,y,z [m]
+        default_joint_angles = { # = target angles [rad] when action = 0.0
+            'leftFrontLegMotor': 0.0,   # [rad]
+            'leftRearLegMotor': 0.0,   # [rad]
+            'rightFrontLegMotor': -0.0 ,  # [rad]
+            'rightRearLegMotor': -0.0,   # [rad]
+
+            'leftFrontUpperLegMotor': 0.0,     # [rad]
+            'leftRearUpperLegMotor': 0.,   # [rad]
+            'rightFrontUpperLegMotor': 0.0,     # [rad]
+            'rightRearUpperLegMotor': 0.,   # [rad]
+
+            'leftFrontLowerLegMotor': -0.0,   # [rad]
+            'leftRearLowerLegMotor': -0.0,    # [rad]
+            'rightFrontLowerLegMotor': -0.0,  # [rad]
+            'rightRearLowerLegMotor': -0.0,    # [rad]
+        }
+
+    class control( LeggedRobotCfg.control ):
+        # PD Drive parameters:
+        control_type = 'P'
+        stiffness = {'LegMotor': 5.}  # [N*m/rad]
+        damping = {'LegMotor': 0.5}     # [N*m*s/rad]
+        # action scale: target angle = actionScale * action + defaultAngle
+        action_scale = 0.25
+        # decimation: Number of control action updates @ sim DT per policy DT
+        decimation = 4
+
+    class asset( LeggedRobotCfg.asset ):
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/pupper/pupper_v2a.urdf'
+        name = "pupper"
+        foot_name = "LowerLeg"
+        penalize_contacts_on = ["UpperLeg"]
+        terminate_after_contacts_on = ["pupper_v2_dji_chassis"]
+        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
+  
+    class rewards( LeggedRobotCfg.rewards ):
+        soft_dof_pos_limit = 0.9
+        base_height_target = 0.25
+        class scales( LeggedRobotCfg.rewards.scales ):
+            torques = -0.0002
+            dof_pos_limits = -10.0
+
+class PupperFlatCfgPPO( LeggedRobotCfgPPO ):
+    class algorithm( LeggedRobotCfgPPO.algorithm ):
+        entropy_coef = 0.01
+    class runner( LeggedRobotCfgPPO.runner ):
+        run_name = ''
+        experiment_name = 'flat_pupper'
+
+  
